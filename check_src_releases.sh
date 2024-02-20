6ca196fb35cfb4fdd9effd99ef13da5258799dc4
Stephan Geulette <s.geulette@imio.be>
2024-02-20 10:18:03 +0100

    Improved check_src_releases.sh script

--- check_src_releases.sh
+++ check_src_releases.sh
@@ -1,19 +1,51 @@
 #!/bin/bash
 
 # Script to be used in a plone buildout to check if development eggs must be released.
-# Must be run in buildout src directory
 # if argument "1" is passed, only changed folders are echoed
 
-if [[ "$PWD" != */src ]];
-then
-    echo "Error: must be run in src directory"
-    exit 1;
-fi
+CHANGED=''
+TOOL=''
+TOOLS="iadocs"
+escaped_iadocs="collective.contact.importexport collective.externaleditor collective.portlet.actions collective.relationhelpers
+                collective.z3cform.select2 imio.dms.mail imio.transmogrifier.contact imio.transmogrifier.iadocs
+                plone.app.robotframework plone.formwidget.datetime Products.LDAPUserFolder transmogrify.dexterity"
+
+usage () {
+    echo "DESCRIPTION: This script must be used on an src buildout directory, to check if development eggs are to be released."
+    echo "USAGE: `basename $0` (-c) (-t iadocs)"
+    echo "  -c = changed folders only are listed."
+    echo "  -h = this help."
+    echo "  -t tool = tool filtering (iadocs)"
+}
+
+while getopts ":cht:" opt; do
+  case $opt in
+    c)  CHANGED='1'; echo "=> CHANGED='$CHANGED'"
+        ;;
+    t)  TOOL="$OPTARG"; echo "=> TOOL='$TOOL'"
+        ;;
+    h)  usage; exit 1;
+        ;;
+    \?) echo "Invalid option -$OPTARG" >&2 ; usage; exit 1
+        ;;
+    :)  echo "Option -$OPTARG requires an argument." >&2; exit 1
+        ;;
+  esac
+done
+
+# check if in src directory
+if [[ "$PWD" != */src ]]; then echo "Error: must be run in src directory"; exit 1; fi
+
+# check if TOOL is valid
+#if [ "$TOOL" != "" ] && ! echo "$TOOLS" | tr " " '\n' |grep -Fqx "$TOOL" ; then echo "Invalid tool option '$TOOL' : must be one of '$TOOLS'"; exit 1; fi
+if [ "$TOOL" != "" ] && ! echo "$TOOLS" | grep -wq "$TOOL" ; then echo "Invalid tool option '$TOOL' : must be one of '$TOOLS'"; exit 1; fi
+ESCAPING="escaped_${TOOL}"
 
 for j in $(ls -1)
 do
     if [ -d $j ] && [ "$j" != appy ]
     then
+      if [ "$TOOL" != "" ] && echo "${!ESCAPING}" | grep -wq "$j"; then continue; fi
       cd $j
       for cf in CHANGES.rst CHANGES.txt docs/HISTORY.rst docs/HISTORY.txt docs/CHANGES.rst nothing;
       do
@@ -31,7 +63,7 @@ do
         # echo $found
         if [ "$found" ]
         then
-          if [ "$1" != "1" ]; then echo "$j/$cf UNCHANGED"; fi
+          if [ "$CHANGED" != "1" ]; then echo "$j/$cf UNCHANGED"; fi
         else
           echo "$j/$cf"
         fi
